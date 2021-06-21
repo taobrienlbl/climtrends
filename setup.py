5bfb79e65b40458644cc5438f8ebb5cc62e710ae
Travis A. O'Brien <obrienta@iu.edu>
2021-06-21 10:05:21 -0400

    Updated repo URL and version #

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="climtrends", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Travis A. O'Brien",
     author_email="obrienta@iu.edu",
     description="A utility for Bayesian trend regression with a variety of statistical models",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.iu.edu/obrienta/climtrends",
+    url="https://github.com/taobrienlbl/climtrends",
     packages=setuptools.find_packages(),
     classifiers=[
     "Programming Language :: Python :: 3",
